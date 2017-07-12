05c7ef268f635d49b369abfaa8e1d5879891d78e
bmhagui <bacem_hagui@hotmail.com>
2017-07-12 13:51:07 +0200

    tested battery saving, pgrep no longer has -i option....didnt work on my pc, compliz needs to be included for ubuntu users

--- battery_test.sh
+++ battery_test.sh
@@ -2,8 +2,21 @@
 
 while [ 1 ]
 do
-xdotool key alt+Tab
-sleep 5
+    xdotool mousemove 1088 37
+    xdotool click 1
+    sleep 5
+    
+    xdotool mousemove 378 41
+    xdotool click 1
+    sleep 5
+    
+    xdotool mousemove 318 688
+    xdotool click 1
+    sleep 5
+    
+    xdotool mousemove 1091 677
+    xdotool click 1
+    sleep 5
 done
 
 #xdotool getmouselocation
